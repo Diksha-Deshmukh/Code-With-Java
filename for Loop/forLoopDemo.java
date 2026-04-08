b0f057b3a5b59ef4220e2bac4d7b97ef1a43547b
Diksha Deshmukh <er.dikshadeshmukh@gmail.com>
2026-04-08 12:56:04 +0100

    Delete for Loop directory

--- for Loop/forLoopDemo.java
+++
@@ -1,9 +0,0 @@
-public class ForLoopDemo {
-    public static void main(String[] args) {
-
-        // Print numbers from 1 to 10
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Number: " + i);
-        }
-    }
-}
